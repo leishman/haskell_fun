1a1224de2e458284c3a67c79dab6d55d357c41b4
Alexander Leishman <leishman3@gmail.com>
2014-07-13 14:09:31 -0700

    make first comprehension

--- fun.hs
+++ fun.hs
@@ -1,3 +1,10 @@
 doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+
+
+length' xs = sum [1 | _ <- xs]
