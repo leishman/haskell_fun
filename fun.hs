68fbd7020c77bbd19ebad4f46a451f4113253dca
Alexander Leishman <leishman3@gmail.com>
2014-07-14 00:14:19 -0700

    custom tuple functions

--- fun.hs
+++ fun.hs
@@ -31,3 +31,17 @@ factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial x = x * factorial (x - 1)
 
+-- adding vectors
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+-- fst (x, y) #=> x
+-- snd (x, y) #=> y
+
+-- there is no defined third function:
+-- let's write it ourselves
+
+third :: (a, b, c) -> c
+third (_, _, z) = z -- '_' means we don't care what it is
+
