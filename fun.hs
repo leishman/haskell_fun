dc73a5da377165c8fd601f2245235d8ab5c86df6
Alexander Leishman <leishman3@gmail.com>
2014-07-14 00:04:51 -0700

    made functions. you can define output for given input. pretty cool

--- fun.hs
+++ fun.hs
@@ -12,3 +12,16 @@ length' xs = sum [1 | _ <- xs]
 ------------------------
 
 -- Some pattern matching
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
+
